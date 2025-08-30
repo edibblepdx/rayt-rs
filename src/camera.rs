42e908cfadb736740eeffe2051c178c40adb9bd8
ethan dibble <edibble@pdx.edu>
2025-08-30 16:17:53 -0700

    feature: camera struct

---
+++ src/camera.rs
@@ -0,0 +1,39 @@
+use crate::types::{Point3, UnitVec3, Vec3};
+
+pub struct Camera {
+    eye: Point3,
+    up: UnitVec3,
+    right: UnitVec3,
+    forward: UnitVec3,
+    viewport_width: f64,
+    viewport_height: f64,
+    focal_length: f64,
+}
+
+impl Camera {
+    /// Returns a new camera with the given parameters.
+    ///
+    /// The forward direction is calculated as `right` cross `up`.
+    pub fn new(
+        eye: Point3,
+        up: Vec3,
+        right: Vec3,
+        viewport_width: f64,
+        viewport_height: f64,
+        focal_length: f64,
+    ) -> Self {
+        let up = UnitVec3::new_normalize(up);
+        let right = UnitVec3::new_normalize(right);
+        let forward = UnitVec3::new_normalize(right.cross(&up));
+
+        Camera {
+            eye: eye.into(),
+            up,
+            right,
+            forward,
+            viewport_width,
+            viewport_height,
+            focal_length,
+        }
+    }
+}
