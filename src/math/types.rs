1bc4e6af25b0c642de6d53dabb995425de5866c4
ethan dibble <edibble@pdx.edu>
2025-09-04 15:45:37 -0700

    docs: Interval

--- src/math/types.rs
+++ src/math/types.rs
@@ -51,27 +51,34 @@ impl Neg for UnitVec3 {
 pub struct Interval(pub f64, pub f64);
 
 impl Interval {
+    /// Equivalent to (+[`INFINITY`], -[`INFINITY`])
     pub const EMPTY: Interval = Interval(INFINITY, -INFINITY);
+    /// Equivalent to (-[`INFINITY`], +[`INFINITY`])
     pub const UNIVERSE: Interval = Interval(-INFINITY, INFINITY);
 
+    /// Returns a new interval `(min, max)`
     pub fn new(min: f64, max: f64) -> Self {
         Interval(min, max)
     }
 
+    /// Returns the size of the interval as `Interval.1 - Interval.0`.
     pub fn size(&self) -> f64 {
         self.1 - self.0
     }
 
+    /// Returns `true` if the interval contains `x` bounds inclusive.
     pub fn contains(&self, x: f64) -> bool {
         (self.0..self.1).contains(&x)
     }
 
+    /// Returns `true` if the interval contains `x` bounds non-inclusive.
     pub fn surrounds(&self, x: f64) -> bool {
         self.0 < x && x < self.1
     }
 }
 
 impl Default for Interval {
+    /// Returns an [`Interval::EMPTY`] interval.
     fn default() -> Interval {
         Interval::EMPTY
     }
