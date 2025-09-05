eb0a53c9432fcaf4f16062aa9b0bd3f3f55151dd
ethan dibble <edibble@pdx.edu>
2025-09-05 00:13:35 -0700

    feature: camera builder

--- src/camera.rs
+++ src/camera.rs
@@ -1,39 +1,185 @@
-use crate::math::types::{Point3, UnitVec3, Vec3};
+use crate::color::*;
+use crate::prelude::*;
 
+use std::{
+    io::{self, Write},
+    sync::Arc,
+};
+
+use indicatif::{ParallelProgressIterator, ProgressIterator, ProgressStyle};
+use rayon::prelude::*;
+
+#[allow(unused)]
 pub struct Camera {
-    pub eye: Point3,
-    pub up: UnitVec3,
-    pub right: UnitVec3,
-    pub forward: UnitVec3,
-    pub viewport_width: f64,
-    pub viewport_height: f64,
-    pub focal_length: f64,
+    image_width: usize,
+    image_height: usize,
+    position: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+    forward: UnitVec3,
+    right: UnitVec3,
+    up: UnitVec3,
 }
 
 impl Camera {
-    /// Returns a new camera with the given parameters.
-    ///
-    /// The forward direction is calculated as `right` cross `up`.
-    pub fn new(
-        eye: Point3,
-        up: Vec3,
-        right: Vec3,
-        viewport_width: f64,
-        viewport_height: f64,
-        focal_length: f64,
-    ) -> Self {
-        let up = UnitVec3::new_normalize(up);
-        let right = UnitVec3::new_normalize(right);
-        let forward = UnitVec3::new_normalize(right.cross(*up));
+    pub fn builder() -> CameraBuilder {
+        CameraBuilder::default()
+    }
+
+    pub fn render(self, world: HittableList) {
+        println!("P3\n{} {}\n255", self.image_width, self.image_height);
+
+        let world = Arc::new(world);
+
+        let image_area = self.image_width * self.image_height;
+        let mut pixels: Vec<Color> = Vec::with_capacity(image_area);
+
+        // safety: look one line up
+        unsafe { pixels.set_len(image_area) };
+
+        let ps = ProgressStyle::with_template(
+            "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>7}/{len:7}",
+        )
+        .unwrap()
+        .progress_chars("#>-");
+
+        log::info!("Generating Image");
+
+        pixels
+            .par_chunks_mut(self.image_width)
+            .progress_with_style(ps.clone())
+            .enumerate()
+            .for_each(|(j, row)| {
+                for i in 0..self.image_width {
+                    let pixel_center = self.pixel00_loc
+                        + (i as f64 * self.pixel_delta_u)
+                        + (j as f64 * self.pixel_delta_v);
+
+                    let ray_direction = UnitVec3::new_normalize(pixel_center - self.position);
+                    let r = Ray::new(self.position, ray_direction);
+
+                    row[i] = Camera::ray_color(&r, &world);
+                }
+            });
+
+        log::info!("Writing Image");
+
+        let mut out = io::BufWriter::new(io::stdout());
+        for pixel_color in pixels.iter().progress_with_style(ps.clone()) {
+            write_color(&mut out, &pixel_color).expect("Failed Write");
+        }
+        out.flush().unwrap();
+    }
+
+    fn ray_color(ray: &Ray, world: &HittableList) -> Color {
+        if let Some(record) = world.hit(ray, (0.0, INFINITY).into()) {
+            let mapped = record.normal.map(|e| (e + 1.0) / 2.0);
+            return Color(mapped);
+        }
+
+        let mut t = ray.direction().y;
+        t = (t + 1.0) / 2.0;
+
+        let start = Color(Vec3::new(1.0, 1.0, 1.0));
+        let end = Color(Vec3::new(0.5, 0.7, 1.0));
+
+        Color((1.0 - t) * start.0 + t * end.0)
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Camera {
+        CameraBuilder::default().build()
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct CameraBuilder {
+    aspect_ratio: f64,
+    image_width: usize,
+    position: Point3,
+    look_at: Vec3,
+    up: Vec3,
+}
+
+impl CameraBuilder {
+    pub fn build(self) -> Camera {
+        // Camera basis.
+        let forward = UnitVec3::new_normalize(self.look_at - self.position);
+        let right = UnitVec3::new_normalize(forward.cross(self.up));
+        let up = UnitVec3::new_normalize(right.cross(*forward));
+
+        // Image Dimensions.
+        let image_width = self.image_width;
+        let mut image_height: usize = (image_width as f64 / self.aspect_ratio) as usize;
+        image_height = if image_height < 1 { 1 } else { image_height };
+
+        // Viewport Dimensions.
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * image_width as f64 / image_height as f64;
+
+        // Viewport uv
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        // Pixel delta uv
+        let pixel_delta_u = viewport_u / image_width as f64;
+        let pixel_delta_v = viewport_v / image_height as f64;
+
+        // Position of upper left pixel.
+        let viewport_upper_left =
+            self.position - Vec3::new(viewport_width / 2.0, -viewport_height / 2.0, focal_length);
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
         Camera {
-            eye: eye.into(),
-            up,
-            right,
+            image_width,
+            image_height,
+            position: self.position,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
             forward,
-            viewport_width,
-            viewport_height,
-            focal_length,
+            right,
+            up,
+        }
+    }
+
+    pub fn aspect_ratio(mut self, aspect: impl Into<f64>) -> Self {
+        self.aspect_ratio = aspect.into();
+        self
+    }
+
+    pub fn image_width(mut self, width: impl Into<usize>) -> Self {
+        self.image_width = width.into();
+        self
+    }
+
+    pub fn position(mut self, pos: impl Into<Vec3>) -> Self {
+        self.position = pos.into();
+        self
+    }
+
+    pub fn look_at(mut self, look_at: impl Into<Vec3>) -> Self {
+        self.look_at = look_at.into();
+        self
+    }
+
+    pub fn up(mut self, vup: impl Into<Vec3>) -> Self {
+        self.up = vup.into();
+        self
+    }
+}
+
+impl Default for CameraBuilder {
+    fn default() -> CameraBuilder {
+        CameraBuilder {
+            aspect_ratio: 1.0,
+            image_width: 100,
+            position: Point3::splat(0.0),
+            look_at: Vec3::NEG_Z,
+            up: Vec3::Y,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,19 @@
-use indicatif::{ParallelProgressIterator, ProgressIterator, ProgressStyle};
-
-use std::{
-    io::{self, Write},
-    sync::Arc,
-};
-
-use rayon::prelude::*;
-
+use rayt_rs::math::primitives::Sphere;
 use rayt_rs::prelude::*;
-use rayt_rs::{
-    color::{Color, write_color},
-    math::primitives::Sphere,
-};
 
 const ASPECT_RATIO: f64 = 16.0 / 9.0;
 
 fn main() {
     env_logger::init();
 
-    // Image
-    // -----
-
-    // Image dimensions are integer-valued.
-    let image_width: usize = 400;
-    let mut image_height: usize = (image_width as f64 / ASPECT_RATIO) as usize;
-    image_height = if image_height < 1 { 1 } else { image_height };
-
-    // World
-    // -----
     let mut world = HittableList::default();
     world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
     world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
 
-    // Camera
-    // ------
+    let camera = Camera::builder()
+        .aspect_ratio(ASPECT_RATIO)
+        .image_width(400usize)
+        .build();
 
-    // Distance from the eye to the image plane.
-    let focal_length = 1.0;
-
-    // Viewport dimensions are real-valued.
-    // The actual aspect ratio may not be ASPECT_RATIO.
-    let viewport_height = 2.0; // arbitrary
-    let viewport_width = viewport_height * image_width as f64 / image_height as f64;
-
-    let eye = Vec3::new(0.0, 0.0, 0.0);
-
-    /*
-    let camera = Camera::new(
-        Point3::new(0.0, 0.0, 0.0),  // eye
-        Vec3::new(0.0, 1.0, 0.0), // up
-        Vec3::new(1.0, 0.0, 0.0), // right
-        viewport_width,
-        viewport_height,
-        focal_length,
-    );
-    */
-
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    let viewport_upper_left =
-        eye - Vec3::new(viewport_width / 2.0, -viewport_height / 2.0, focal_length);
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-    // ------
-
-    println!("P3\n{image_width} {image_height}\n255");
-
-    let world = Arc::new(world);
-
-    let image_area = image_width * image_height;
-    let mut pixels: Vec<Color> = Vec::with_capacity(image_area);
-    // safety: look one line up
-    unsafe { pixels.set_len(image_area) };
-
-    let ps = ProgressStyle::with_template(
-        "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos:>7}/{len:7}",
-    )
-    .unwrap()
-    .progress_chars("#>-");
-
-    log::info!("Generating Image");
-
-    pixels
-        .par_chunks_mut(image_width)
-        .progress_with_style(ps.clone())
-        .enumerate()
-        .for_each(|(j, row)| {
-            for i in 0..image_width {
-                let pixel_center =
-                    pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-
-                let ray_direction = UnitVec3::new_normalize(pixel_center - eye);
-                let r = Ray::new(eye, ray_direction);
-
-                row[i] = ray_color(&r, &world);
-            }
-        });
-
-    log::info!("Writing Image");
-
-    let mut out = io::BufWriter::new(io::stdout());
-    for pixel_color in pixels.iter().progress_with_style(ps.clone()) {
-        write_color(&mut out, &pixel_color).expect("Failed Write");
-    }
-    out.flush().unwrap();
-}
-
-fn ray_color(ray: &Ray, world: &HittableList) -> Color {
-    if let Some(record) = world.hit(ray, (0.0, INFINITY).into()) {
-        let mapped = record.normal.map(|e| (e + 1.0) / 2.0);
-        return Color(mapped);
-    }
-
-    let mut t = ray.direction().y;
-    t = (t + 1.0) / 2.0;
-
-    let start = Color(Vec3::new(1.0, 1.0, 1.0));
-    let end = Color(Vec3::new(0.5, 0.7, 1.0));
-
-    Color((1.0 - t) * start.0 + t * end.0)
+    camera.render(world);
 }
