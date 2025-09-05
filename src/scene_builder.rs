cf5de1a5b865d236f1272096b6f941320141db01
ethan dibble <edibble@pdx.edu>
2025-09-05 01:43:39 -0700

    feature: add custom Error type for SceneBuilder

--- src/scene_builder.rs
+++ src/scene_builder.rs
@@ -7,9 +7,12 @@ use serde::Deserialize;
 pub struct SceneBuilder;
 
 impl SceneBuilder {
-    pub fn build(path: impl AsRef<Path>) -> anyhow::Result<(Camera, HittableList)> {
-        let toml_str = fs::read_to_string(path)?;
-        let config: Config = toml::from_str(&toml_str)?;
+    pub fn build(path: impl AsRef<Path>) -> Result<(Camera, HittableList), Error> {
+        let toml_str =
+            fs::read_to_string(path).map_err(|err| Error::FileReadError(err.to_string()))?;
+
+        let config: Config = toml::from_str(&toml_str)
+            .map_err(|err| Error::ConfigDeError(err.message().to_string()))?;
 
         let camera = config.camera.build();
         let mut world = HittableList::default();
@@ -32,3 +35,20 @@ struct Config {
 struct Primitive {
     sphere: Vec<Sphere>,
 }
+
+#[derive(Debug)]
+pub enum Error {
+    FileReadError(String),
+    ConfigDeError(String),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Error::FileReadError(s) => write!(f, "File read error: {}", s),
+            Error::ConfigDeError(s) => write!(f, "Config deserialization error: {}", s),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
